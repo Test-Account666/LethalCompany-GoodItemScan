d7ddb4314ef5e7d901de654b93a218e74892e8d1
TestAccount666 <36412486+Test-Account666@users.noreply.github.com>
2025-01-02 14:01:44 +0100

    Add config option relating to scanning landmines

--- GoodItemScan/ConfigManager.cs
+++ GoodItemScan/ConfigManager.cs
@@ -31,50 +31,48 @@ public static class ConfigManager {
 
     public static ConfigEntry<float> updateTimer = null!;
 
+    public static ConfigEntry<bool> fixLandmineScanNode = null!;
+
 
     internal static void Initialize(ConfigFile configFile) {
         preferClosestNodes = configFile.Bind("General", "Prefer Closest Nodes", true,
-                                             "If true, will prefer scanning the closest nodes first. "
-                                           + "This might cause performance issues.");
+                                             "If true, will prefer scanning the closest nodes first. " + "This might cause performance issues.");
 
         alwaysRescan = configFile.Bind("General", "Always Rescan", true,
-                                       "If true, will always start a fresh scan. "
-                                     + "This removes all previously scanned nodes from the UI.");
+                                       "If true, will always start a fresh scan. " + "This removes all previously scanned nodes from the UI.");
 
         scanNodesHardLimit = configFile.Bind("General", "Scan Nodes Hard Limit", 120,
-                                             new ConfigDescription("Defines the maximum amount of scan nodes on screen. "
-                                                                 + "If you feel like your screen is cluttered, try lowering this value.",
-                                                                   new AcceptableValueRange<int>(30, 666)));
+                                             new ConfigDescription(
+                                                 "Defines the maximum amount of scan nodes on screen. "
+                                               + "If you feel like your screen is cluttered, try lowering this value.", new AcceptableValueRange<int>(30, 666)));
 
         scanNodeDelay = configFile.Bind("General", "Scan Node Delay", 0.1F,
-                                        new ConfigDescription("Defines the delay between each scan node being added to the UI. "
-                                                            + "This will look stupid if set too high. "
-                                                            + "This value is divided by 100.",
-                                                              new AcceptableValueRange<float>(0, 1F)));
+                                        new ConfigDescription(
+                                            "Defines the delay between each scan node being added to the UI. " + "This will look stupid if set too high. "
+                                          + "This value is divided by 100.", new AcceptableValueRange<float>(0, 1F)));
 
         useDictionaryCache = configFile.Bind("General", "Use Dictionary Cache", true,
-                                             "May increase performance, at the cost of ram usage. "
-                                           + "If true, will use a dictionary for caching. "
-                                           + "If false, will not cache at all.");
+                                             "May increase performance, at the cost of ram usage. " + "If true, will use a dictionary for caching. "
+                                                                                                    + "If false, will not cache at all.");
 
         alwaysCheckForLineOfSight = configFile.Bind("General", "Always Check For Line Of Sight", false,
                                                     "If true, will check for line of sight every frame. "
                                                   + "Enabling this could cause performance issues. Vanilla value is true.");
 
         maxScanNodesToProcessPerFrame = configFile.Bind("General", "Max Scan Nodes To Process Per Frame", 32,
-                                                        new ConfigDescription("This value defines how many scan nodes can be processed each frame."
-                                                                            + "This value is used in updating and scanning nodes!",
-                                                                              new AcceptableValueRange<int>(1, 666)));
+                                                        new ConfigDescription(
+                                                            "This value defines how many scan nodes can be processed each frame."
+                                                          + "This value is used in updating and scanning nodes!", new AcceptableValueRange<int>(1, 666)));
 
-        updateScanNodeText = configFile.Bind("General", "Update Scan Node Text", true, "If true, will update the scan node text. "
-                                                                                     + "This is useful, if you, for example, have a mod that updates the value of an item.");
+        updateScanNodeText = configFile.Bind("General", "Update Scan Node Text", true,
+                                             "If true, will update the scan node text. "
+                                           + "This is useful, if you, for example, have a mod that updates the value of an item.");
 
-        calculateDistance = configFile.Bind("General", "Calculate Distance", false, "If true, will perform additional distance calculations. "
-                                                                                  + "If false, use viewport as distance. "
-                                                                                  + "Set to `true`, if you don't mind slightly worse performance and want accurate distance calculations.");
+        calculateDistance = configFile.Bind("General", "Calculate Distance", false,
+                                            "If true, will perform additional distance calculations. " + "If false, use viewport as distance. "
+                                                                                                       + "Set to `true`, if you don't mind slightly worse performance and want accurate distance calculations.");
 
-        sendDebugMessages = configFile.Bind("Debug", "Send Debug Messages", false,
-                                            "If set to true, will spam your log with debug messages.");
+        sendDebugMessages = configFile.Bind("Debug", "Send Debug Messages", false, "If set to true, will spam your log with debug messages.");
 
         showOpenedBlastDoorScanNode = configFile.Bind("Special Cases", "Show opened blast door scan node", true,
                                                       "If set to true, will allow you to scan nodes of opened blast doors (Vanilla value: false) "
@@ -92,14 +90,14 @@ public static class ConfigManager {
                                                  new ConfigDescription(
                                                      "This multiplier is used to define the wait time between adding more to the total displayed value. "
                                                    + "The lower this number, the faster the animation is updated."
-                                                   + "The lower this number, the less will be added per updated."
-                                                   + "For a vanilla-ish feeling, set this to 48.",
+                                                   + "The lower this number, the less will be added per updated." + "For a vanilla-ish feeling, set this to 48.",
                                                      new AcceptableValueRange<int>(1, 100)));
 
         updateTimer = configFile.Bind("Special Cases", "Scan Node Update Timer", 1.2F,
                                       new ConfigDescription(
                                           "This value is used to have a cooldown between scan node updates (Like position updates). "
-                                        + "The lower this number, the more often it will get updated.",
-                                          new AcceptableValueRange<float>(0F, 12F)));
+                                        + "The lower this number, the more often it will get updated.", new AcceptableValueRange<float>(0F, 12F)));
+
+        fixLandmineScanNode = configFile.Bind("Special Cases", "Fix Landmine ScanNode", true, "Fixes Landmines being scannable after exploding.");
     }
 }
